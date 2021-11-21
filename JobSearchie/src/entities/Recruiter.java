251a7f300f8882e17ababc2b6394980fc64fa08d
Tim <tper0026@student.monash.edu>
2021-11-21 22:03:28 +1100

    JJJJJJJJob seeker and recruiter entities

--- JobSearchie/src/entities/Recruiter.java
+++ JobSearchie/src/entities/Recruiter.java
@@ -1,3 +1,62 @@
 package entities;
 
-public class Recruiter extends User{}
+import java.util.ArrayList;
+
+public class Recruiter extends User{
+    private String companyName;
+    private String recruitingSpecialty;
+    private ArrayList<Job> jobListings;
+    private ArrayList<Invitation> invitationsOffered;
+
+    public Recruiter(){
+        super();
+        companyName = "";
+        recruitingSpecialty = "";
+        jobListings = new ArrayList<Job>();
+        invitationsOffered = new ArrayList<Invitation>();
+    }
+
+    public Recruiter(String companyName, String recruitingSpecialty, ArrayList<Job> jobListings, ArrayList<Invitation> invitationsOffered) {
+        super();
+        this.companyName = companyName;
+        this.recruitingSpecialty = recruitingSpecialty;
+        this.jobListings = jobListings;
+        this.invitationsOffered = invitationsOffered;
+    }
+
+    public void display(){
+
+    }
+    public String getCompanyName() {
+        return companyName;
+    }
+
+    public ArrayList<Invitation> getInvitationsOffered() {
+        return invitationsOffered;
+    }
+
+    public ArrayList<Job> getJobListings() {
+        return jobListings;
+    }
+
+    public String getRecruitingSpecialty() {
+        return recruitingSpecialty;
+    }
+
+    public void setCompanyName(String companyName) {
+        this.companyName = companyName;
+    }
+
+    public void setInvitationsOffered(ArrayList<Invitation> invitationsOffered) {
+        this.invitationsOffered = invitationsOffered;
+    }
+
+    public void setJobListings(ArrayList<Job> jobListings) {
+        this.jobListings = jobListings;
+    }
+
+    public void setRecruitingSpecialty(String recruitingSpecialty) {
+        this.recruitingSpecialty = recruitingSpecialty;
+    }
+
+}
