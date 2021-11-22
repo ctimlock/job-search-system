312be42746ad8b0b2a0318bfeb641d6ee2640b99
lqui007 <lqui0007@student.monash.edu>
2021-11-23 08:43:13 +1100

    Basic database functionality. Welcome screen and start up now working. Further work on the database is required to get the login/register to work with database interaction.

--- JobSearchie/src/database/DatabaseIO.java
+++ JobSearchie/src/database/DatabaseIO.java
@@ -2,12 +2,58 @@ package database;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.*;
 
 public class DatabaseIO {
-    private String cwd;
+    private File file;
+
+    public DatabaseIO(File file) {
+        this.file = file;
+    }
+
+    public ArrayList<HashMap<String, String>> getLines() {
+        ArrayList<HashMap<String, String>> resultSet = new ArrayList<>();
+        ArrayList<String> header = new ArrayList<>();
+        try (Scanner sc = new Scanner(file)){
+            if (sc.hasNextLine()) {
+                Arrays
+                    .asList(
+                        sc.nextLine().split(","))
+                    .forEach(element ->
+                        header.add(element.strip()));
+            } else {
+                return null;
+            }
+            while (sc.hasNextLine()) {
+                String[] line = sc.nextLine().split(",");
+                HashMap<String, String> result = new HashMap<>();
+                for (int i = 0; i < line.length; i++) {
+                    result.put(header.get(i), line[i]);
+                }
+                resultSet.add(result);
+            }
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+        return resultSet;
+    }
+
+    public void printLines() {
+        try (Scanner sc = new Scanner(file)){
+            while (sc.hasNextLine())
+                System.out.println(sc.nextLine());
+        } catch(Exception e) {
+            e.printStackTrace();
+        }
+    }
 
 
-    public DatabaseIO() throws IOException {
-        cwd = new String(new File(".").getCanonicalPath());
+
+    public File getFile() {
+        return file;
+    }
+
+    public void setFile(File file) {
+        this.file = file;
     }
 }
