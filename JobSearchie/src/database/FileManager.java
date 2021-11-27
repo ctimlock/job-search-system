fb70d1c9b9e5b00829d36be6ac781b2487899028
Charlie Timlock <ctim0003@student.monash.edu>
2021-11-27 20:28:49 +1100

    Added the ability to open a file from within a JS directory. Added the FileOpeningTest class inside /Testing Changed FileManager.java to have the file storage root folder as a static attribute.

--- JobSearchie/src/database/FileManager.java
+++ JobSearchie/src/database/FileManager.java
@@ -1,8 +1,11 @@
 package database;
 
+import utilities.UserIO;
+
 import javax.swing.*;
 import javax.swing.filechooser.FileNameExtensionFilter;
 import javax.swing.filechooser.FileSystemView;
+import java.awt.*;
 import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
@@ -10,13 +13,39 @@ import java.nio.file.StandardCopyOption;
 
 /**
  * @author Charlie Timlock
- * @version 1.0
+ * @version 2.0
  * This class handles file transfer for the Job Searchie system, and allows for uploading and downloading files from the
  * Job Searchie file storage system.
  */
 
 public abstract class FileManager
 {
+    private static final String FILE_ROOT_DIRECTORY = "JobSearchie/database/fileStorage/";
+
+
+    /**
+     * This method checks whether the requested file exists within the directory provided.
+     * @param directory The directory to be accessed within the JS file storage.
+     * @param fileName The file name, including the extension.
+     * @return Returns true when the file exists.
+     */
+    public static Boolean checkFileExists(String directory, String fileName)
+    {
+        File target = new File(FILE_ROOT_DIRECTORY + directory + "/" + fileName);
+        return target.exists();
+    }
+
+    /**
+     * This method returns an array of all files within a given directory in the JobSearchie fileStorage.
+     * @param directory The selected directory.
+     * @return The names of all files inside the given directory, as an array of Strings.
+     */
+    public static String[] listFilesFromJSDirectory(String directory)
+    {
+        File folder = new File(FILE_ROOT_DIRECTORY + directory + "/");
+        return folder.list();
+    }
+
     /**
      * This method will copy a file from a given path into a specified directory in the JobSearchie fileStorage directory.
      * @param directory The selected directory.
@@ -30,8 +59,23 @@ public abstract class FileManager
             File src = new File(srcPath);
             int index = src.getName().lastIndexOf('.');
             newFileName += src.getName().substring(index);
-            File dest = new File("JobSearchie/database/fileStorage/" + directory + "/" + newFileName);
+            File dest = new File(FILE_ROOT_DIRECTORY + directory + "/" + newFileName);
             Files.copy(src.toPath(), dest.toPath(), StandardCopyOption.REPLACE_EXISTING);
+        }
+    }
+
+    /**
+     * This method locates a file withing a JobSearchie file storage directory, and opens it if it is available.
+     * @param directory The directory to be accessed within the JS file storage.
+     * @param fileName The file name, including the extension.
+     * @throws IOException Will throw if the file is not found.
+     */
+    public static void openFileFromJS(String directory, String fileName) throws IOException
+    {
+        File f = new File(FILE_ROOT_DIRECTORY + directory + "/" + fileName);
+        if (f.exists())
+        {
+            Desktop.getDesktop().open(f);
         }
     }
 
@@ -42,7 +86,7 @@ public abstract class FileManager
      * @param dialogueTitle The dialogue box's title.
      * @param newFileName The selected name for the new file, excluding the file extension.
      * @param fileTypes A whitelist of selectable file types. Any file type not in this list will not appear inside the selection window.
-     * @throws IOException
+     * @throws IOException Will throw if the file is not found.
      */
     public static void selectFileAndMoveToJSStorage(String directory, String dialogueTitle, String newFileName, String... fileTypes) throws IOException
     {
