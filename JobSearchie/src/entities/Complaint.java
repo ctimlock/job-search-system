66d55c1b47a8a6a46b55811769cd95b93fcd934c
Charlie Timlock <ctim0003@student.monash.edu>
2021-11-27 16:15:14 +1100

    Fixed several build issues and fixed .gitignore again

--- JobSearchie/src/entities/Complaint.java
+++ JobSearchie/src/entities/Complaint.java
@@ -1,3 +1,25 @@
 package entities;
 
-public class Complaint {}
+public class Complaint
+{
+    private String complaintID;
+    public Complaint()
+    {
+        complaintID = "00000";
+    }
+
+    public Complaint(String complaintID)
+    {
+        this.complaintID = complaintID;
+    }
+
+    public String getComplaintID()
+    {
+        return complaintID;
+    }
+
+    public void setComplaintID(String complaintID)
+    {
+        this.complaintID = complaintID;
+    }
+}
--- JobSearchie/src/entities/Location.java
+++ JobSearchie/src/entities/Location.java
@@ -1,54 +1,76 @@
 package entities;
 
-public class Location {
+public class Location
+{
     private String country;
     private String state;
     private String city;
+    private String suburb;
     private String postcode;
 
-    public Location() {
+    public Location()
+    {
         country = "";
         state = "";
         city = "";
         postcode = "";
     }
 
-    public Location(String country, String state, String city, String postcode) {
+    public Location(String country, String state, String city, String postcode)
+    {
         this.country = country;
         this.state = state;
         this.city = city;
         this.postcode = postcode;
     }
 
-    public String getCountry() {
+    public String getSuburb()
+    {
+        return suburb;
+    }
+
+    public void setSuburb(String suburb)
+    {
+        this.suburb = suburb;
+    }
+
+    public String getCountry()
+    {
         return country;
     }
 
-    public void setCountry(String country) {
+    public void setCountry(String country)
+    {
         this.country = country;
     }
 
-    public String getState() {
+    public String getState()
+    {
         return state;
     }
 
-    public void setState(String state) {
+    public void setState(String state)
+    {
         this.state = state;
     }
 
-    public String getCity() {
+    public String getCity()
+    {
         return city;
     }
 
-    public void setCity(String city) {
+    public void setCity(String city)
+    {
         this.city = city;
     }
 
-    public String getPostcode() {
+    public String getPostcode()
+    {
         return postcode;
     }
 
-    public void setPostcode(String postcode) {
+    public void setPostcode(String postcode)
+    {
         this.postcode = postcode;
     }
 }
