e2ccae3252e868748fa605c0f5efcde541e5b132
lqui007 <lqui0007@student.monash.edu>
2021-11-28 17:56:03 +1100

    initial commit

---
+++ JobSearchie/src/database/UserDB.java
@@ -0,0 +1,21 @@
+package database;
+
+public class UserDB {
+    public static final String TABLE_NAME = "user";
+    public static final String ID = "id";
+    public static final String ACCOUNTTYPE = "accountType";
+    public static final String FIRSTNAME = "firstName";
+    public static final String LASTNAME = "lastName";
+    public static final String EMAIL = "email";
+    public static final String PASSWORD = "password";
+    public static final String LOCATIONID = "locationId";
+    public static final String CONTACTNUMBER = "contactNumber";
+    public static final String DATECREATED = "dateCreated";
+    public static final String DATEOFBIRTH = "dateOfBirth";
+    public static final String CURRENTJOBNAME = "currentJobName";
+    public static final String CURRENTJOBLEVEL = "currentJobLevel";
+    public static final String USER_COLUMN_EXPECTEDCOMPENSATION = "expectedCompensation";
+    public static final String USER_COLUMN_RESUMEDIR = "resumeDir";
+    public static final String USER_COLUMN_COMPANYNAME = "companyName";
+    public static final String USER_COLUMN_RECRUITINGSPECIALTY = "recruitingSpecialty";
+}
