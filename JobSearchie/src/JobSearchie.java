8b7dd8aad3727bb517d8ae488b7b4d730fff7ec4
Tim <tper0026@student.monash.edu>
2021-12-03 11:09:52 +1100

    Documentation for jobSearchie and Validate classes.

--- JobSearchie/src/JobSearchie.java
+++ JobSearchie/src/JobSearchie.java
@@ -6,12 +6,22 @@ import entities.Session;
 import javax.swing.*;
 import java.sql.SQLException;
 
+/**
+ * This is the driver class of the program.
+ * @author  Team R
+ * @version 1.0
+ */
 public class JobSearchie
 {
     private Session session;
     private DatabaseManager db;
     private JFrame frameInit;
 
+    /**
+     * This is the main method which begins the program execution.
+     * @param args  An array of string passed in as command line parameters.
+     * @throws SQLException If there is an access error with the database.
+     */
     public static void main(String[] args) throws SQLException
     {
         JobSearchie program = new JobSearchie();
@@ -23,6 +33,9 @@ public class JobSearchie
         //program.run();
     }
 
+    /**
+     *
+     */
     public void run() {
         DatabaseManager db = new DatabaseManager();
 
@@ -31,6 +44,9 @@ public class JobSearchie
         db.close();
     }
 
+    /**
+     * This method closes the database and terminates the program.
+     */
     public void exit() {
         db.close();
         System.exit(1);
