7d5024acab2f7b1f20383ac1510fc371656d2b27
mngu0105 <mngu0105@student.monash.edu>
2021-12-03 13:58:29 +1100

    Source code documentation for handler classes.

--- JobSearchie/src/Controllers/UserHandler.java
+++ JobSearchie/src/Controllers/UserHandler.java
@@ -1,5 +1,11 @@
 package Controllers;
 
+/**
+ * Controller class for the User entity.
+ *
+ * @author Charlie Timlock, Levi Quilliam, Tim Perkins, and Merrill Nguyen
+ * @version ver1.0.0
+ */
 public abstract class UserHandler
 {
 }
