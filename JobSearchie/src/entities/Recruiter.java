1caa9c3e84ee2b242ec1ce85cb27bdb147aa2f99
Tim <tper0026@student.monash.edu>
2021-12-03 12:22:51 +1100

    Documentation for jobSearchie and Validate classes.

--- JobSearchie/src/entities/Recruiter.java
+++ JobSearchie/src/entities/Recruiter.java
@@ -2,12 +2,22 @@ package entities;
 
 import java.sql.Date;
 
+/**
+ * An entity class (child class of User class) which stores information for the recruiter user by extending
+ * the user class.
+ *
+ * @author  Team R
+ * @version 1.0
+ */
 public class Recruiter extends User {
     private String companyName;
     private String recruitingSpecialty;
     private String contactNumber;
     private Date dateOfBirth;
 
+    /**
+     * Default constructor which creates the object of the class Recruiter partly by extending the User class.
+     */
     public Recruiter() {
         super("Recruiter");
         companyName = "";
@@ -16,6 +26,11 @@ public class Recruiter extends User {
         dateOfBirth = null;
     }
 
+    /**
+     * Non-default constructor which creates the object of the class Recruiter partly by extending the User class.
+     *
+     * @param email Accepts the recruiter's email as a string.
+     */
     public Recruiter(String email) {
         super("Recruiter", email);
         companyName = "";
@@ -24,6 +39,15 @@ public class Recruiter extends User {
         dateOfBirth = null;
     }
 
+    /**
+     * Non-default constructor which creates the object of the class Recruiter partly by extending the User class.
+     *
+     * @param firstName Accepts the recruiter's first name as a string.
+     * @param lastName  Accepts the recruiter's last name as a string.
+     * @param email Accepts the recruiter's email address as a string.
+     * @param password  Accepts the recruiter's password as a string.
+     * @param dateCreated   Accepts the recruiter's profile creation date as a Date object datatype.
+     */
     public Recruiter(String firstName, String lastName, String email, String password, Date dateCreated) {
         super("Recruiter", firstName, lastName, email, password, dateCreated);
         companyName = "";
@@ -32,6 +56,19 @@ public class Recruiter extends User {
         dateOfBirth = null;
     }
 
+    /**
+     * Non-default constructor which creates the object of the class Recruiter partly by extending the User class.
+     *
+     * @param firstName Accepts the recruiter's first name as a string.
+     * @param lastName  Accepts the recruiter's last name as a string.
+     * @param email Accepts the recruiter's email address as a string.
+     * @param password  Accepts the recruiter's password as a string.
+     * @param dateCreated   Accepts the recruiter's profile creation date as a Date object datatype.
+     * @param companyName   Accepts the recruiter's company name as a string.
+     * @param recruitingSpecialty   Accepts the recruiter's recruiting specialty as a string.
+     * @param contactNumber Accepts the recruiter's contact number as a string.
+     * @param dateOfBirth   Accepts the recruiter's date of birth as a Date object datatype.
+     */
     public Recruiter(String firstName, String lastName, String email, String password, Date dateCreated, String companyName, String recruitingSpecialty, String contactNumber, Date dateOfBirth) {
         super("Recruiter", firstName, lastName, email, password, dateCreated);
         this.companyName = companyName;
@@ -40,6 +77,9 @@ public class Recruiter extends User {
         this.dateOfBirth = dateOfBirth;
     }
 
+    /**
+     * Display method to print the state of the object.
+     */
     public void display() {
         super.display();
         System.out.println("companyName: " + companyName);
@@ -48,34 +88,67 @@ public class Recruiter extends User {
         System.out.println("dateOfBirth: " + dateOfBirth);
     }
 
+    /**
+     * Accessor method to get the recruiter's company name.
+     *
+     * @return  The company name as a string.
+     */
     public String getCompanyName() {
         return companyName;
     }
 
+    /**
+     *
+     * @return
+     */
     public String getContactNumber() {
         return contactNumber;
     }
 
+    /**
+     *
+     * @return
+     */
     public Date getDateOfBirth() {
         return dateOfBirth;
     }
 
+    /**
+     *
+     * @return
+     */
     public String getRecruitingSpecialty() {
         return recruitingSpecialty;
     }
 
+    /**
+     *
+     * @param companyName
+     */
     public void setCompanyName(String companyName) {
         this.companyName = companyName;
     }
 
+    /**
+     *
+     * @param contactNumber
+     */
     public void setContactNumber(String contactNumber) {
         this.contactNumber = contactNumber;
     }
 
+    /**
+     *
+     * @param dateOfBirth
+     */
     public void setDateOfBirth(Date dateOfBirth) {
         this.dateOfBirth = dateOfBirth;
     }
 
+    /**
+     *
+     * @param recruitingSpecialty
+     */
     public void setRecruitingSpecialty(String recruitingSpecialty) {
         this.recruitingSpecialty = recruitingSpecialty;
     }
--- JobSearchie/src/entities/User.java
+++ JobSearchie/src/entities/User.java
@@ -2,6 +2,12 @@ package entities;
 
 import java.sql.Date;
 
+/**
+ * An entity class which stores information for the user.
+ *
+ * @author  Team R
+ * @version 1.0
+ */
 public class User {
     private String accountType;
     private String firstName;
@@ -10,6 +16,9 @@ public class User {
     private String password;
     private Date dateCreated;
 
+    /**
+     * Default constructor which creates the object of the User class.
+     */
     public User() {
         accountType = "User";
         firstName = "";
@@ -19,6 +28,11 @@ public class User {
         dateCreated = null;
     }
 
+    /**
+     * Non-default constructor which creates the object of the User class
+     *
+     * @param accountType   Accepts the user's account type as a String.
+     */
     public User(String accountType) {
         this.accountType = accountType;
         firstName = "";
@@ -28,6 +42,12 @@ public class User {
         dateCreated = null;
     }
 
+    /**
+     * Non-default constructor which creates the object of the User class
+     *
+     * @param accountType   Accepts the user's account type as a String.
+     * @param email Accepts the user's email as a String.
+     */
     public User(String accountType, String email) {
         this.accountType = accountType;
         firstName = "";
@@ -37,6 +57,13 @@ public class User {
         dateCreated = null;
     }
 
+    /**
+     * Non-default constructor which creates the object of the User class
+     *
+     * @param email Accepts the user's email as a String.
+     * @param password  Accepts the user's password as a String.
+     * @param dateCreated   Accepts the user's profile creation date as a Date object datatype.
+     */
     public User(String email, String password, Date dateCreated) {
         this.accountType = null;
         this.firstName = null;
@@ -46,6 +73,16 @@ public class User {
         this.dateCreated = dateCreated;
     }
 
+    /**
+     * Non-default constructor which creates the object of the User class
+     *
+     * @param accountType   Accepts the user's account type as a String.
+     * @param firstName Accepts the user's first name as a String.
+     * @param lastName  Accepts the user's last name as a String.
+     * @param email Accepts the user's email as a String.
+     * @param password  Accepts the user's password as a String.
+     * @param dateCreated   Accepts the administrators profile creation date as a Date object datatype.
+     */
     public User(String accountType, String firstName, String lastName, String email, String password, Date dateCreated) {
         this.accountType = accountType;
         this.firstName = firstName;
@@ -55,6 +92,9 @@ public class User {
         this.dateCreated = dateCreated;
     }
 
+    /**
+     * Display method to print the state of the object.
+     */
     public void display() {
         System.out.println("accountType: " + accountType);
         System.out.println("firstName: " + firstName);
@@ -64,50 +104,110 @@ public class User {
         System.out.println("dateCreated: " + dateCreated);
     }
 
+    /**
+     * Accessor method to get the user's account type.
+     *
+     * @return  The account type as a string.
+     */
     public String getAccountType() {
         return accountType;
     }
 
+    /**
+     * Mutator method to set the user's account type.
+     *
+     * @param accountType   The user's account type as a String.
+     */
     public void setAccountType(String accountType) {
         this.accountType = accountType;
     }
 
+    /**
+     * Accessor method to get the user's account creation date.
+     *
+     * @return  The account creation date as a string.
+     */
     public Date getDateCreated() {
         return dateCreated;
     }
 
+    /**
+     * Accessor method to get the user's email address.
+     *
+     * @return  The user's email as a string.
+     */
     public String getEmail() {
         return email;
     }
 
+    /**
+     * Accessor method to the get user's first name.
+     *
+     * @return  The user's first name as a string.
+     */
     public String getFirstName() {
         return firstName;
     }
 
+    /**
+     * Accessor method to the get user's last name.
+     *
+     * @return  The user's last name as a string.
+     */
     public String getLastName() {
         return lastName;
     }
 
+    /**
+     * Accessor method to the get user's password.
+     *
+     * @return  The user's password as a string.
+     */
     public String getPassword() {
         return password;
     }
 
+    /**
+     * Mutator method to set the user's account creation date.
+     *
+     * @param dateCreated   The user's account creation date as a string.
+     */
     public void setDateCreated(Date dateCreated) {
         this.dateCreated = dateCreated;
     }
 
+    /**
+     * Mutator method to set the user's email.
+     *
+     * @param email The user's email as a string.
+     */
     public void setEmail(String email) {
         this.email = email;
     }
 
+    /**
+     * Mutator method to set the user's first name.
+     *
+     * @param firstName The user's first name as a string.
+     */
     public void setFirstName(String firstName) {
         this.firstName = firstName;
     }
 
+    /**
+     * Mutator method to set the user's last name.
+     *
+     * @param lastName  The user's last name as a string.
+     */
     public void setLastName(String lastName) {
         this.lastName = lastName;
     }
 
+    /**
+     * Mutator method to set the users password.
+     *
+     * @param password  The user's password as a string.
+     */
     public void setPassword(String password) {
         this.password = password;
     }
